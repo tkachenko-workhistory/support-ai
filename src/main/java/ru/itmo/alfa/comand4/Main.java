5884bb520cb7ea41713c001d119c4ebe0c16f6b0
tkachenko-learn <ower-7+learn@yandex.ru>
2025-12-03 15:15:30 +0500

    delete мёртвый код

--- src/main/java/ru/itmo/alfa/comand4/Main.java
+++ src/main/java/ru/itmo/alfa/comand4/Main.java
@@ -8,27 +8,5 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        /*try {
-            // Загрузка данных
-            List<SupportTicket> tickets = new CsvTicketSource("C:\\Users\\Иван\\Desktop\\tech_support_dataset.csv").getAllTicket();
-            TicketProcessor processor = new TicketProcessor(tickets);
-
-            // Обработка новой заявки
-            TicketRequest newTicket = new TicketRequest(
-                    "Cannot connect to Wi-Fi",
-                    "Laptop sees networks but cannot connect to internet"
-            );
-
-            TicketResponse response = processor.processNewTicket(newTicket);
-
-            // Вывод результатов
-            System.out.println("Категория: " + response.getCategory());
-            System.out.println("Рекомендации:");
-            response.getRecommendedSolutions().forEach(s -> System.out.println(" • " + s));
-
-        } catch (Exception e) {
-            System.err.println("Ошибка: " + e.getMessage());
-            e.printStackTrace();
-        }*/
     }
 }
